e37c349d5334a1afb4fc39bf90f85a91aeede854
Yuma Soga <ysoga19@gmail.com>
2022-11-17 16:15:51 +0900

    fix typo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,9 +12,9 @@ const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    seporia: {
+    sepolia: {
       url: "https://eth.ymsg.space/v1/sepolia",
-      accounts: [DEPLOYER_PRIVATE_KEY ? DEPLOYER_PRIVATE_KEY : ""]
+      accounts: DEPLOYER_PRIVATE_KEY ? [DEPLOYER_PRIVATE_KEY] : undefined
     }
   }
 };
